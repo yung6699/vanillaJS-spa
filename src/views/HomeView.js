8a025709a26e2b9364b07b6f82341b890ca89a6e
Yoon <yty3592@gmail.com>
2020-10-18 20:41:19 +0900

    HomeView 개선
    - setState 메소드 추가
    - API 호출 후 state에 저장 및 각 컴포넌트로 전달하는 기능 추가

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,20 +1,44 @@
 import AbstractView from "./AbstractView.js";
 import HomeContainer from "../containers/HomeContainer.js";
+import { fetchMovies } from '/src/api/index.js'
+import { propsStateToComponents } from '/src/utils/components.js'
 
 const HOME_VIEW_TITLE = 'Home';
+const MOVIES = 'movies';
+const HOME_CONTAINER = 'homeContainer';
 
 export default class HomeView extends AbstractView{
   constructor(params) {
     super();
-    this.init();
-  }
 
-  init () {
-    const target = this.$viewElement;
     this.setTitle(HOME_VIEW_TITLE);
-    this.setComponents('homeContainer', new HomeContainer(target));
+    this.initComponent();
+    this.initState();
+    this.fetchMovies();
+    propsStateToComponents(this.components, this.state);
   }
 
+  initComponent () {
+    const target = this.$viewElement;
+    this.setComponents(HOME_CONTAINER, new HomeContainer(target));
+  }
+
+  initState () {
+    this.state = {
+      movies: []
+    };
+  }
+
+  setState (key, value) {
+    this.state[key] = value;
+    propsStateToComponents(this.components, this.state);
+  }
+
+  async fetchMovies () {
+    const { data } = await fetchMovies({ pageNum: 1 });
+    const { movies } = data;
+    this.setState(MOVIES, [ ...movies ])
+  }
 
   render () {
     const { homeContainer } = this.components;
